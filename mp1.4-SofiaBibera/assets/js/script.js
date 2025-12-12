458a6cedef6e5049d683e9b1404f63fe16dc987e
Sofia Margaret Bibera <smBibera@mcm.edu.ph>
2025-12-12 23:50:04 +0800

    Delete mp1.4-SofiaBibera directory

--- mp1.4-SofiaBibera/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btnCount").addEventListener("click", function () {
-    
-    let text = document.getElementById("txtInput").value.trim();
-    let output = document.getElementById("txtOutput");
-
-    let count = text.length;
-
-    output.textContent = count;
-});
